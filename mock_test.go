da2688a2e94cf73bbb093225237056362edd7d2d
mahi <manivenkatasairam.thotakura@zopsmart.com>
2022-01-20 16:25:27 +0530

    Mocked db

--- mock_test.go
+++ mock_test.go
@@ -1,195 +1,184 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
+	//"database/sql"
+	//	"fmt"
+	//"reflect"
+	//"fmt"
+	"testing"
 
-	"github.com/go-sql-driver/mysql"
+	"github.com/DATA-DOG/go-sqlmock"
 )
 
-var db *sql.DB
-
-func New(d *sql.DB) {
-	db = d
-}
-
-type Album struct {
-	ID     int64
-	Title  string
+/*type Album struct{
+	ID int64
+	Title string
 	Artist string
-	Price  float64
-}
-
-/*type Bowler struct{
-	Name string
-	Age int64
-	Wickets int64
+	Price float32
 }*/
 
-func main() {
-	cfg := mysql.Config{
-		User:   "root",
-		Passwd: "",
-		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-		DBName: "recordings",
-	}
-	var err error
-	db, err = sql.Open("mysql", cfg.FormatDSN())
+// func Test_Create(t *testing.T){
+
+// 	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+// 	if err != nil {
+// 		t.Fatalf("an error '%s' was not expected while opening a stub database connection", err)
+// 	}
+// 	defer db.Close()
+//     New(db)
+
+// 	mock.ExpectExec("CREATE TABLE album if not exists(id INT AUTO_INCREMENT NOT NULL,title VARCHAR(128) NOT NULL,artist VARCHAR(255) NOT NULL,price DECIMAL(5,2) NOT NULL,PRIMARY KEY (`id`));").WillReturnResult(sqlmock.NewResult(1,1)),
+
+// }
+
+func Test_Create(t *testing.T) {
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("an error '%s' was not expected while opening a stub database connection", err)
 	}
+	defer db.Close()
+	New(db)
 
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
+	mock.ExpectExec("CREATE TABLE album if not exists(id INT AUTO_INCREMENT NOT NULL,title VARCHAR(128) NOT NULL,artist VARCHAR(255) NOT NULL,price DECIMAL(5,2) NOT NULL,PRIMARY KEY (`id`))").WillReturnResult(sqlmock.NewResult(1, 1))
+
+	if err = create(); err != nil {
+		t.Errorf("ellehe:%s", err)
 	}
-	fmt.Println("Connected")
-	
-	err=create()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	albID, err := addAlbum(Album{
-		Title:  "The Modern Sound of Betty Carter",
-		Artist: "Betty Carter",
-		Price:  49.99,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("ID of added album: %v\n", albID)
-
-    err=updateAlbum(2)
-	if err!=nil{
-		fmt.Printf("Update failed")
-	}
-
-	albums, err := albumsByArtist("John Coltrne")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Albums fund: %v\n", albums)
-
-	err=delete(3)
-	if err!=nil{
-		fmt.Printf("Update failed")
-	}
-
-	alb, err := albumByID(1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Album found: %v\n", alb)
-
-	
-
-	
-
-
-	/*bowler := []Bowler{
-		{Name: "Lord_Shardhul", Wickets: 200, Age: 27},
-		{Name: "Snake_Bhajji", Wickets: 10, Age: 57},
-		{Name: "SwingKing_Chahar", Wickets: 100, Age: 27},
-	}
-	addBowlers(bowler)
-
-	updateBowlers("Snake_Bhajji")*/
 }
-
-/*func updateBowlers(name string) error {
-	_, err := db.Exec("Update Bowler set wickets=5 where name=?", name)
+func Test_Addalbum(t *testing.T) {
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	if err != nil {
-		fmt.Println("Update failed")
-		return err
+		t.Fatalf("an error '%s' was not expected while opening a stub database connection", err)
 	}
-	return nil
+	defer db.Close()
+	New(db)
 
-}*/
-
-func create() error{
-	_, err := db.Exec("CREATE TABLE album if not exists(id INT AUTO_INCREMENT NOT NULL,title VARCHAR(128) NOT NULL,artist VARCHAR(255) NOT NULL,price DECIMAL(5,2) NOT NULL,PRIMARY KEY (`id`))")
-
-	if err != nil {
-		return err
+	tests := []struct {
+		desc   string
+		id     int
+		Title  string
+		Artist string
+		Price  float64
+		resp   int64
+		err    error
+		mock   []interface{}
+	}{
+		{
+			desc:   "case-1",
+			Title:  "sirivennela",
+			Artist: "Anurag",
+			Price:  100.09,
+			resp:   1,
+			err:    nil,
+			mock: []interface{}{
+				mock.ExpectExec("INSERT INTO album(title,artist,price) VALUES (?,?,?)").WithArgs("sirivennela", "Anurag", 100.09).WillReturnResult(sqlmock.NewResult(1, 1)),
+			},
+		},
 	}
-	return nil
-}
 
-func updateAlbum(id int) error{
-	_,err:=db.Exec("Update Album set Price=79.99 where id=?",id)
-	if err!=nil{
-        //fmt.Println("Update failed")
-		return err
-	}
-	return nil
-}
-
-func delete(id int) error{
-	_,err:=db.Exec("Delete From Album where id=?",id)
-	if err!=nil{
-        //fmt.Println("Update failed")
-		return err
-	}
-	return nil
-}
-
-func albumsByArtist(name string) ([]Album, error) {
-	var albums []Album
-	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
-	if err != nil {
-		return nil, fmt.Errorf("albumsbyArtist %q: %v", name, err)
-
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var alb Album
-		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-			return nil, fmt.Errorf("albumsbyArtist %q: %v", name, err)
-		}
-		albums = append(albums, alb)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("albumsbyArtist %q: %v", name, err)
-	}
-	return albums, nil
-}
-
-func albumByID(id int64) (Album, error) {
-	var alb Album
-	row := db.QueryRow("SELECT * FROM album WHERE id=?", id)
-	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-		if err == sql.ErrNoRows {
-			return alb, fmt.Errorf("no albums found")
-		}
-		return alb, fmt.Errorf("albumByID:%d: %v", id, err)
-
-	}
-	return alb, nil
-}
-
-func addAlbum(alb Album) (int64, error) {
-	//fmt.Println(alb)
-	result, err := db.Exec("INSERT INTO album(title,artist,price) VALUES (?,?,?)", alb.Title, alb.Artist, alb.Price)
-	if err != nil {
-		return 0, fmt.Errorf("addA;bum: %v", err)
-	}
-	id, err := result.LastInsertId()
-	if err != nil {
-		return 0, fmt.Errorf("ehey")
-	}
-	return id, nil
-}
-
-/*func addBowlers(bowlers []Bowler) {
-	for _, bowler := range bowlers {
-		_, err := db.Exec("Insert into Bowler(Name,Age,Wickets) VALUES (?,?,?)", bowler.Name, bowler.Age, bowler.Wickets)
-		if err != nil {
-			fmt.Printf("ehey")
+	// album:=Album{ID:15,Title:"Kalla Bolli",Artist:"Harris Jayraj",Price: 30.0}
+	for _, tc := range tests {
+		resp, err := addAlbum(Album{Title: tc.Title, Artist: tc.Artist, Price: tc.Price})
+		//fmt.Println(resp)
+		//fmt.Println(tc.resp)
+		if resp != tc.resp && err != tc.err {
+			t.Errorf("error was not expected while updating stats: %v", err)
 		}
 
 	}
 
-}*/
+}
+
+func Test_update(t *testing.T) {
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected while opening a stub database connection", err)
+	}
+	defer db.Close()
+	New(db)
+
+	tests := []struct {
+		desc string
+		id   int
+
+		resp error
+
+		mock []interface{}
+	}{
+		{
+			desc: "case-1",
+			id:   1,
+			resp: nil,
+
+			mock: []interface{}{
+				mock.ExpectExec("Update Album set Price=79.99 where id=?").WithArgs(1).WillReturnResult(sqlmock.NewResult(1, 1)),
+			},
+		},
+	}
+	//mock.ExpectExec("Update Album set Price=79.99 where id=?").WithArgs(2).WillReturnResult(sqlmock.NewResult(1,1))
+	for _, tc := range tests {
+		err := updateAlbum(tc.id)
+		//fmt.Println(err)
+		//fmt.Println(tc.resp)
+		if err != tc.resp {
+			t.Errorf("%v", tc.resp)
+		}
+	}
+
+}
+
+func Test_delete(t *testing.T) {
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected while opening a stub database connection", err)
+	}
+	defer db.Close()
+	New(db)
+
+	tests := []struct {
+		desc string
+		id   int
+
+		resp error
+
+		mock []interface{}
+	}{
+		{
+			desc: "case-1",
+			id:   3,
+			resp: nil,
+
+			mock: []interface{}{
+				mock.ExpectExec("Delete From Album where id=?").WithArgs(3).WillReturnResult(sqlmock.NewResult(1, 1)),
+			},
+		},
+	}
+	//mock.ExpectExec("Update Album set Price=79.99 where id=?").WithArgs(2).WillReturnResult(sqlmock.NewResult(1,1))
+	for _, tc := range tests {
+		err := delete(tc.id)
+		//fmt.Println(err)
+		//fmt.Println(tc.resp)
+		if err != tc.resp {
+			t.Errorf("%v", tc.resp)
+		}
+	}
+
+}
+
+func Test_ID(t *testing.T) {
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected while opening a stub database connection", err)
+	}
+	defer db.Close()
+	New(db)
+	rows := sqlmock.NewRows([]string{"ID", "Title", "Artist", "Price"}).
+		//AddRow(2,"Giant Steps", "John Coltrane", 63.99).
+		//AddRow(1,"Blue Train", "John Coltrane", 56.99).
+		AddRow(1, "Sarah Vaughan", "Sarah vaughan", 34.98)
+	mock.ExpectQuery("SELECT * FROM album WHERE id=?").WithArgs(1).WillReturnRows(rows)
+
+	if _, err = albumByID(1); err != nil {
+		t.Errorf("error was not expected while updating stats: %s", err)
+	}
+
+}
