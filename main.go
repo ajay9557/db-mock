0a10f30a3e5b2f42088bdb424f51b98b17c24f16
TejasMD22 <tejas.md@zopsmart.com>
2022-01-21 09:23:58 +0530

    added mock test files

---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Student struct {
+	id            int
+	name, address string
+}
+
+type SqlDB struct {
+	db *sql.DB
+}
+
+func connectDb() (*sql.DB, error) {
+	connection, error := sql.Open("mysql", "root:password@tcp(127.0.0.1)/test")
+	if error != nil {
+		return nil, errors.New("DATABASE CONNECTION ERROR")
+	}
+	return connection, nil
+}
+
+func (db *SqlDB) InsertStudent(id int, name string, address string) error {
+	_, error := db.db.Exec("insert into student(id, name, address) values(?, ?, ?)", id, name, address)
+
+	if id < 0 {
+		return errors.New("STUDENT ID IS INVALID")
+	}
+
+	if error != nil {
+		return errors.New("FAILED TO ADD NEW STUDENT")
+	}
+	return nil
+}
+
+func (db *SqlDB) DeleteStudent(id int) error {
+	_, error := db.db.Exec("delete from student where id=?", id)
+
+	if id < 0 {
+		return errors.New("FAILED TO DELETE THE STUDENT")
+	}
+
+	if error != nil {
+		return errors.New("STUDENT NOT EXISTS")
+	}
+	return nil
+}
+
+func (db *SqlDB) UpdateStudent(id int, name string, address string) error {
+	_, error := db.db.Exec("update student set name=?, address=? where id=?", name, address, id)
+
+	if id < 0 {
+		return errors.New("INVALID STUDENT ID")
+	}
+
+	if error != nil {
+		return errors.New("FAILED TO UPDATE STUDENT")
+	}
+	return nil
+}
+
+func (db *SqlDB) SelectStudentById(id int) (Student, error) {
+	stud := Student{}
+	con := db.db.QueryRow("select * from student where id=?", id)
+
+	error := con.Scan(&stud.id, &stud.name, &stud.address)
+
+	if error != nil {
+		return stud, errors.New("ERROR IN SELECTING")
+	}
+
+	return stud, nil
+}
+
+func (db *SqlDB) SelectAllStudents() ([]Student, error) {
+	StudentList := make([]Student, 10)
+	con, error := db.db.Query("select * from student")
+
+	if error != nil {
+		return StudentList, errors.New("ERROR IN SELECTING STUDENTS")
+	}
+
+	defer con.Close()
+
+	for con.Next() {
+		stud := Student{}
+		error := con.Scan(&stud.id, &stud.name, &stud.address)
+		if error != nil {
+			return StudentList, errors.New("ERROR IN SELECTING STUDENTS")
+		}
+		StudentList = append(StudentList, stud)
+	}
+	return StudentList, nil
+}
+
+func main() {
+	connection, _ := connectDb()
+	dbconn := SqlDB{connection}
+	/*
+		var n int
+
+		fmt.Println("1.Insert")
+		fmt.Println("2.Select")
+		fmt.Println("3.Select Rows By Id")
+		fmt.Println("4.Update")
+		fmt.Println("5.Delete")
+		fmt.Printf("\nEnter Your Choice :")
+		fmt.Scanln(&n)
+
+		switch n {
+		case 1:
+			fmt.Println(dbconn.InsertStudent(9, "sanjay", "Banglore"))
+		case 2:
+			fmt.Println(dbconn.SelectStudentById(1))
+		case 3:
+			fmt.Println(dbconn.SelectStudentById(4))
+		case 4:
+			fmt.Println(dbconn.UpdateStudent(4, "suhas", "pune"))
+		case 5:
+			fmt.Println(dbconn.DeleteStudent(2))
+		default:
+			fmt.Println("Enter a valid option.")
+		}
+
+	*/
+	// fmt.Println(dbconn.InsertStudent(4, "madhu", "Banglore"))
+	fmt.Println(dbconn.SelectStudentById(4))
+	// fmt.Println(dbconn.UpdateStudent(4, "suhas", "pune"))
+	// fmt.Println(dbconn.DeleteStudent(4))
+}
